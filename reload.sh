15e03b613d25235eacd5e004f17a259e19188839
Gabriel Vigliensoni <gabriel@starr.local>
2018-08-09 14:37:59 -0400

    Fix: Refresh .inputrc.

--- reload.sh
+++ reload.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Copy files over.
-cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/ && ./.inputrc ~/
+cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/ && cp ./.inputrc ~/
 
 # Refresh the settings.
 . ~/.aliases
 . ~/.bash_prompt
 . ~/.bash_profile
 . ~/.functions
-. ~/.inputrc
+bind -f ~/.inputrc
 
 # Update all brew packages, and cleanup old versions.
 brew upgrade
